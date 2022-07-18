2307c295e036bbc1defc1a2e31b6d3a868031d2e
rajish <maharjanrajish101@gmail.com>
2022-07-18 21:45:29 +0545

    urls for CRUD

--- student/urls.py
+++ student/urls.py
@@ -1,17 +1,16 @@
 from operator import imod
 from django.urls import path
 
-from student.views import student_detail
-from .views import create_student,update_student
+from student.views import create_student, homeview,update_student,student_detail,student_list,homeview,delete_student
 
 app_name = 'student'
 
 urlpatterns = [
-    # path('',create_student.CreateStudent.as_view())
-    # path('',create_student.AddView.as_view())
+    path('',homeview.Home, name = 'home'),
     path('create_student',create_student.create_student, name = 'create_student'),
     path('update_student/<str:id>',update_student.updateStudent, name = 'update_student'),
     path('detail_student/<str:id>',student_detail.studentDetail, name = 'detail_student'),
-
+    path('student_list',student_list.studentList, name = 'student_list'),
+    path('delete_student/<str:id>',delete_student.deleteStudent,name = 'delete_student')
 
 ]
