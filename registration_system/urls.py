963c632d08eaf8b8658957f48f5bd09f43286f32
rajish <maharjanrajish101@gmail.com>
2022-07-18 21:43:49 +0545

    routes added

--- registration_system/urls.py
+++ registration_system/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('student.urls'))
+    path('',include('student.urls')),
+    path('batch',include('batch.urls'))
 ]
 
 
