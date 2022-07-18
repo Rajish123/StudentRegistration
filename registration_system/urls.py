ca955c475aeb85821531317baa94566c81774294
rajish <maharjanrajish101@gmail.com>
2022-07-18 22:34:36 +0545

    urls for batch app

--- registration_system/urls.py
+++ registration_system/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('student.urls')),
-    path('batch',include('batch.urls'))
+    path('batch/',include('batch.urls'))
 ]
 
 
