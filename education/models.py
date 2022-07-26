5f0ff2dfba2c35292ac2770dceefa35e6d026df2
rajish <maharjanrajish101@gmail.com>
2022-07-26 19:47:12 +0545

    EducationInformation model created

---
+++ education/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from student.models import Student
+from django.core.exceptions import ObjectDoesNotExist
+
+
+# Create your models here.
+class EducationInformation(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    symbol_number = models.CharField(max_length=50)
+    board = models.CharField(max_length=50)
+    institution_name  = models.CharField(max_length=50)
+    cgpa = models.CharField(max_length=50)
+    passed_year = models.CharField(max_length=50)
+
+    class Meta:
+        ordering = ('symbol_number',)
+        verbose_name = "EducationInformation"
+        verbose_name_plural = "EducationInformations"
+
+    @staticmethod
+    def get_all_educationinfo():
+        return EducationInformation.objects.all()
+
+    @staticmethod
+    def get_educationinfo_by_id(id):
+        try:
+            return EducationInformation.objects.get(id = id)
+        except ObjectDoesNotExist:
+            return None
