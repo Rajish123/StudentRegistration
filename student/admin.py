7db89181a96ecaaf705d6e71250265457ed9c730
rajish <maharjanrajish101@gmail.com>
2022-07-26 19:46:24 +0545

    admin model for student

--- student/admin.py
+++ student/admin.py
@@ -5,3 +5,7 @@ from .models import *
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     list_display = ['name','email','mobile','citizenship','gender','blood_group','temp_address','perm_address','dob','is_active','is_alumi','picture']
+
+
+
+
