213525fa5ea764ab7195465893798c4ac54efec7
rajish <maharjanrajish101@gmail.com>
2022-07-26 19:45:20 +0545

    routing added

--- student/urls.py
+++ student/urls.py
@@ -2,7 +2,6 @@ from operator import imod
 from django.urls import path
 
 from student.views import create_student, homeview,update_student,student_detail,student_list,homeview,delete_student
-
 app_name = 'student'
 
 urlpatterns = [
@@ -11,6 +10,9 @@ urlpatterns = [
     path('update_student/<str:id>',update_student.updateStudent, name = 'update_student'),
     path('detail_student/<str:id>',student_detail.studentDetail, name = 'detail_student'),
     path('student_list',student_list.studentList, name = 'student_list'),
-    path('delete_student/<str:id>',delete_student.deleteStudent,name = 'delete_student')
+    path('delete_student/<str:id>',delete_student.deleteStudent,name = 'delete_student'),
 
+
+
+ 
 ]
