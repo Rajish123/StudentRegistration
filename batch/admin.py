06d4526c2db22f148ce25a2502695eb9c9dda4ba
rajish <maharjanrajish101@gmail.com>
2022-07-26 19:42:12 +0545

    batch admin view

--- batch/admin.py
+++ batch/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from batch.models import Batch
+from batch.models import Batch,Semester,Faculty
 
 # Register your models here.
 @admin.register(Batch)
 class StudentAdmin(admin.ModelAdmin):
     list_display = ['batch_name',]
+
+@admin.register(Semester)
+class SemesterAdmin(admin.ModelAdmin):
+    list_display = ['semester_name',]
+
+@admin.register(Faculty)
+class FacultyAdmin(admin.ModelAdmin):
+    list_display = ['faculty_name',]
+
