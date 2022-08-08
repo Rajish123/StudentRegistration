3a13bdcd0f9862574775e8b97f84d25b379e9fe9
rajish <maharjanrajish101@gmail.com>
2022-08-08 20:17:10 +0545

    CRUD

--- student/views/student_list.py
+++ student/views/student_list.py
@@ -5,5 +5,6 @@ from django.shortcuts import render
 def studentList(request):
     context = {}
     student_list = Student.get_all_student()
+    # eduinfo = student_list.educationinformation_set.all()
     context ={'students':student_list} 
     return render(request, 'student/student_list.html', context)
